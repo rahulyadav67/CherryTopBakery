a5e0ca42497b774fd42e4a0d3b306644b5891de8
sriram1303 <sriram.ravipudi@gmail.com>
2022-08-17 21:20:44 +0530

    Repository

---
+++ CherryTopBakery/Controllers/IteamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CherryTopBakery.Controllers
+{
+    public class IteamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
