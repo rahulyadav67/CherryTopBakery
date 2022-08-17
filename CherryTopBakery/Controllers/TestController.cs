3f72d6bf588ea444bc01e15e69d4e74599fc10d6
rahulyadav67 <rahul.yadav9@globallogic.com>
2022-08-17 17:33:51 +0530

    Added TestController.

---
+++ CherryTopBakery/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CherryTopBakery.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
