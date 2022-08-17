41fd411bea9f35e9704bf322936313edbc074f7e
CakeShopProject <ry67543212@gmail.com>
2022-08-17 17:25:11 +0530

    Add project files.

---
+++ CherryTopBakery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CherryTopBakery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
